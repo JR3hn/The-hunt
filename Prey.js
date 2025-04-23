d26b5dc000b88f729d0e85f311c60eaa76377972
Linus Reichherzer <linus@n149-p203.eduroam.kth.se>
2025-04-23 09:50:19 +0200

    Added Prey.js

---
+++ Prey.js
@@ -0,0 +1,194 @@
+class Prey {
+  static map = null;
+  static INITIAL_LIFE = 20;
+  static INITIAL_ENERGY = 0;
+  
+  constructor(x, y) {
+    this.currentX = x;
+    this.currentY = y;
+    this.hasActed = false;
+    this.lifeSpan = Prey.INITIAL_LIFE;
+    this.energy = Prey.INITIAL_ENERGY;
+    this.predatorNearby = false;
+  }
+  
+  resetTurn() {
+    this.hasActed = false;
+  }
+  
+  hasActed() {
+    return this.hasActed;
+  }
+  
+  static setMap(mainMap) {
+    Prey.map = mainMap;
+  }
+  
+  getCurrentX() {
+    return this.currentX;
+  }
+  
+  getCurrentY() {
+    return this.currentY;
+  }
+  
+  getEnergy() {
+    return this.energy;
+  }
+  
+  setEnergy(toValue) {
+    this.energy = toValue;
+  }
+  
+  killed() {
+    this.lifeSpan = 0;
+    this.energy = 0;
+    Prey.map.incrementPreyEaten();
+  }
+  
+  isDead() {
+    return this.lifeSpan <= 0;
+  }
+  
+  move() {
+    const newX = this.currentX + Math.floor(Math.random() * 3) - 1;
+    const newY = this.currentY + Math.floor(Math.random() * 3) - 1;
+    
+    if (Prey.map.inBounds(newX, newY)) {
+      Prey.map.moveEntity(this.currentX, this.currentY, newX, newY, this);
+      this.currentX = newX;
+      this.currentY = newY;
+    }
+  }
+  
+  escape() {
+    this.predatorNearby = false;
+    
+    for (let i = -2; i <= 2; i++) {
+      for (let j = -2; j <= 2; j++) {
+        const targetX = this.currentX + i;
+        const targetY = this.currentY + j;
+        
+        if (Prey.map.inBounds(targetX, targetY)) {
+          const entities = Prey.map.getEntitiesAt(targetX, targetY);
+          for (const entity of entities) {
+            if (entity.constructor.name === 'Predator') {
+              this.predatorNearby = true;
+              break;
+            }
+          }
+        }
+        if (this.predatorNearby) break;
+      }
+      if (this.predatorNearby) break;
+    }
+    
+    if (this.predatorNearby) {
+      let escapeX = this.currentX;
+      let escapeY = this.currentY;
+      
+      const closestPredator = this.findClosestPredator();
+      if (closestPredator.getCurrentX() > this.currentX) escapeX--;
+      else if (closestPredator.getCurrentX() < this.currentX) escapeX++;
+      if (closestPredator.getCurrentY() > this.currentY) escapeY--;
+      else if (closestPredator.getCurrentY() < this.currentY) escapeY++;
+      
+      if (Prey.map.inBounds(escapeX, escapeY)) {
+        Prey.map.moveEntity(this.currentX, this.currentY, escapeX, escapeY, this);
+        this.currentX = escapeX;
+        this.currentY = escapeY;
+        this.lifeSpan -= 1;
+      }
+    }
+  }
+  
+  findClosestPredator() {
+    let closestPredator = null;
+    let closestDistance = Number.MAX_VALUE;
+    
+    for (let i = -2; i <= 2; i++) {
+      for (let j = -2; j <= 2; j++) {
+        const targetX = this.currentX + i;
+        const targetY = this.currentY + j;
+        
+        if (Prey.map.inBounds(targetX, targetY)) {
+          const entities = Prey.map.getEntitiesAt(targetX, targetY);
+          for (const entity of entities) {
+            if (entity.constructor.name === 'Predator') {
+              const predator = entity;
+              const distance = Math.sqrt(
+                Math.pow(predator.getCurrentX() - this.currentX, 2) +
+                Math.pow(predator.getCurrentY() - this.currentY, 2)
+              );
+              
+              if (distance < closestDistance) {
+                closestDistance = distance;
+                closestPredator = predator;
+              }
+            }
+          }
+        }
+      }
+    }
+    return closestPredator;
+  }
+  
+  turn() {
+    if (this.hasActed) {
+      return;
+    }
+    this.move();
+    this.eat();
+    this.age();
+    // Uncomment the next line to enable escape behavior
+    // this.escape();
+    this.hasActed = true;
+  }
+  
+  consumeEnergy() {
+    if (this.energy > 0) {
+      this.energy--;
+    } else {
+      this.lifeSpan--;
+    }
+  }
+  
+  age() {
+    if (this.lifeSpan > 0) {
+      this.lifeSpan--;
+    } else {
+      this.energy = 0;
+    }
+  }
+  
+  static procreate(x, y) {
+    Prey.map.incrementPreyBorn();
+    if (Prey.map.inBounds(x - 4, y - 4)) {
+      return new Prey(x - 4, y - 4);
+    } else {
+      return new Prey(x + 4, y + 4);
+    }
+  }
+  
+  eat() {
+    const entitiesAtCurrentPosition = Prey.map.getEntitiesAt(this.currentX, this.currentY);
+    for (const entity of entitiesAtCurrentPosition) {
+      if (entity instanceof Grass) {
+        this.energy += 1;
+        entity.consume();
+        entity.resetNextGrow();
+        Prey.map.incrementGrassEaten();
+        break;
+      }
+    }
+  }
+  
+  rest() {
+    if (!this.predatorNearby) {
+      this.energy += 2;
+      this.hasActed = true;
+    }
+  }
+}
+
+export default Prey;
